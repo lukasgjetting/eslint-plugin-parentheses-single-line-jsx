a8bc0b4cbeabfa5d88f7e5449216a01adb768670
Lukas Gjetting <lukasgjetting@gmail.com>
2022-03-23 12:16:59 +0100

    Implement the actual rule

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,34 @@ import { Rule } from 'eslint';
 
 const parenthesesSingleLineJsx = (context: Rule.RuleContext) : Rule.RuleListener => {
     return {
-        ImportDeclaration(node) {
+        JSXElement(node: Rule.Node) {
+            // Multiline JSX is not our responsibility
+            if (node.loc.start.line !== node.loc.end.line) {
+                return;
+            }
+
+            // If element is nested inside another element, we don't need to handle it
+            // TypeScript does not know about JSXElement, so we have to disable it
+            // @ts-ignore
+            if (node.parent.type === 'JSXElement') {
+                return;
+            }
+
+            const sourceCode = context.getSourceCode();
+
+            const tokenBefore = sourceCode.getTokenBefore(node);
+            const tokenAfter = sourceCode.getTokenAfter(node);
+
+            // Node is already parenthesised
+            if (tokenBefore.value === '(' && tokenAfter.value === ')') {
+                return;
+            }
+
             context.report({
                 node,
-                message: 'Lol?',
+                message: 'Missing parentheses around single line JSX. ',
             });
+
         },
     };
 };
